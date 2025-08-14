58864ff65cc5a379261faba2af15a8fa8d2f6de6
Muhammad Yusuf <yuvsufn@gmail.com>
2025-08-14 21:33:26 +0500

    Update customAxios.ts

--- src/api/customAxios.ts
+++ src/api/customAxios.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 
 export const customAxios = axios.create({
-  baseURL: "https://ed0dce324f2e.ngrok-free.app/api",
+  baseURL: "https://67e6cb20a333.ngrok-free.app/api",
   withCredentials: true,
 });
